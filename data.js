3bf330de4b0bc0273bc5c66c14d77e0dcf5bd5e9
proxyht <caohoangtung2001@gmail.com>
2019-02-08 23:47:51 +0700

    first commit

---
+++ data.js
@@ -0,0 +1,38 @@
+var myChart = echarts.init(document.getElementById('trend'));
+
+/*option = {
+    xAxis: {
+        type: 'category',
+        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+    },
+    yAxis: {
+        type: 'value'
+    },
+    series: [{
+        data: [820, 932, 901, 934, 1290, 1330, 1320],
+        type: 'line',
+        smooth: true
+    }]
+};*/
+
+option = {
+    title: {
+        text: 'Basic Area Chart'
+        //text: 'Amortization'
+    },
+    xAxis: {
+        type: 'category',
+        boundaryGap: false,
+        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+    },
+    yAxis: {
+        type: 'value'
+    },
+    series: [{
+        data: [820, 932, 901, 934, 1290, 1330, 1320],
+        type: 'line',
+        //areaStyle: {}
+    }]
+};
+
+myChart.setOption(option);
